09c4ee60d9e56b082a83c0cc7d2c5007081b0c51
Adz2323 <146026081+Adz2323@users.noreply.github.com>
2024-09-17 19:43:16 +0100

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -13,37 +13,38 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 #ifndef HASHTABLEH
 #define HASHTABLEH
 
 #include <string>
 #include <vector>
-#include "SECPK1/Int.h"
+#include "Int.h"
 #ifdef WIN64
 #include <Windows.h>
 #endif
 
 #define HASH_SIZE_BIT 25
-#define HASH_SIZE (1<<HASH_SIZE_BIT)
-#define HASH_MASK (HASH_SIZE-1)
+#define HASH_SIZE (1 << HASH_SIZE_BIT)
+#define HASH_MASK (HASH_SIZE - 1)
 
 #define B0MASK 0xFFFFFFF0ULL
 
 // 36bit for offset, MAX 2^36 point in HashTable
 
-typedef struct {
+typedef struct
+{
   uint32_t b0; // LSB key (28 bit) + HSB Offset (4 bit)
   uint32_t p;  // LSB Offset
 } ENTRY;
 
-class HashTable {
+class HashTable
+{
 
 public:
-
   HashTable();
-  void Add(Int *x, uint64_t  p);
+  void Add(Int *x, uint64_t p);
   bool Get(Int *x, std::vector<uint64_t> &p);
   uint64_t GetNbItem();
   void Reset();
@@ -52,11 +53,9 @@ public:
   void Sort(uint32_t h);
 
 private:
-
-  ENTRY    *E[HASH_SIZE];  
-  uint16_t  M[HASH_SIZE];  // Max
-  uint16_t  C[HASH_SIZE];  // Count
-
+  ENTRY *E[HASH_SIZE];
+  uint16_t M[HASH_SIZE]; // Max
+  uint16_t C[HASH_SIZE]; // Count
 };
 
 #endif // HASHTABLEH
